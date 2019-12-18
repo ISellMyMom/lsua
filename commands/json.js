0eab99fb8c0c56f25b23e2219e512d90bf736cb4
HaZZe <32200147+HaZZeAdv@users.noreply.github.com>
2019-12-18 09:49:40 +0200

    Create json.js

---
+++ commands/json.js
@@ -0,0 +1,32 @@
+const api = "http://hazze.cf/dbdiscord/test";
+const snekfetch = require("snekfetch");
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+
+    snekfetch.get(api).then(r => {
+        let body = r.body;
+        let id = Number(args[0]);                     
+        if(!id) return message.channel.send("Insereaza un ID.");
+        if(isNaN(id)) return message.channel.send("ID invalid");
+        
+        let entry = body.find(post => post.id === id);
+        if(!entry) return message.channel.send("Nu exista");
+        
+        let embed = new Discord.RichEmbed()
+        .setDescription(enter.body)
+        .setColor("#15f153")
+        .addField("Autor", entry.userId)
+        .setFooter("Post ID: " + entry.id);
+
+    message.channel.send({embed:embed});                      
+    });
+}
+
+module.exports.help = {
+    name: "json",
+    type: "info",
+    desc: "Displays an json file from my site.",
+    usage: "json <id>"
+}
