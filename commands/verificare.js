30b58a0fa3ed30b553b4e3958c6a54e6727f8ab9
HaZZe <32200147+HaZZeAdv@users.noreply.github.com>
2019-12-18 16:21:22 +0200

    Update verificare.js

--- commands/verificare.js
+++ commands/verificare.js
@@ -10,11 +10,10 @@ module.exports.run = async (bot, message, args) => {
   let NewBie = new Discord.RichEmbed()
   .setAuthor("Welcome", bot.user.avatarURL)
   .setDescription("Bine ai venit pe serverul nostru de discord, pentru a putea accesa celelalte canale trebuie să citeşti regulamentul şi câteva informaţii despre server.")
-  .addField("Regulament", "** 1.**  Zuzule zi ce sa scriu aici, pali-te-ar foame.")
-  //.addField("Regulament", "** 1.**  Nu este permisă jignirea directă sau indirectă a unei persoane indiferent de rasă, sex, aspect, națiune, religie, vârstă și tot ce ține de persoana respectivă - __Warn/Mute/Kick/Ban__.\n** 2.**  Nu distribuiți link-uri dăunatoare, reclame fără aprobare sau imagini inadecvate - __Warn/Kick/Ban__.\n** 3.**  Nu este permisă reclama la alt server de discord fără acordul unui administrator - __Ban__.\n** 4.**  Chatul este un loc unde să vă exprimați părerile/concepțiile despre orice, în niciun caz un loc pentru certuri.\n**5.** Este interzis să faceți SPAM, spre exemple:\n- Mesaje scurte şi frecvente.\n- Mesaje cu publicităţi şi link-uri de orice tip.\n- Orice link pus oriunde spre alte servere/site-uri care nu sunt în parteneriat cu noi.")
-  //.addField("Informaţii suplimentare", `La acumularea a 3 Warn-uri primeşti ban.\nToţi membrii serverului, inclusiv membrii staff-ului trebuie să respecte regulamentul.\nPentru a avea acces la tot serverul apasă pe ${agree} de sub mesaj.`)
+  .addField("Regulament", "** 1.**  Nu este permisă jignirea directă sau indirectă a unei persoane indiferent de rasă, sex, aspect, națiune, religie, vârstă și tot ce ține de persoana respectivă - __Warn/Mute/Kick/Ban__.\n** 2.**  Nu distribuiți link-uri dăunatoare, reclame fără aprobare sau imagini inadecvate - __Warn/Kick/Ban__.\n** 3.**  Nu este permisă reclama la alt server de discord fără acordul unui administrator - __Ban__.\n** 4.**  Chatul este un loc unde să vă exprimați părerile/concepțiile despre orice, în niciun caz un loc pentru certuri.\n**5.** Este interzis să faceți SPAM, spre exemple:\n- Mesaje scurte şi frecvente.\n- Mesaje cu publicităţi şi link-uri de orice tip.\n- Orice link pus oriunde spre alte servere/site-uri care nu sunt în parteneriat cu noi.")
+  .addField("Informaţii suplimentare", `La acumularea a 3 Warn-uri primeşti ban.\nToţi membrii serverului, inclusiv membrii staff-ului trebuie să respecte regulamentul.\nPentru a avea acces la tot serverul apasă pe ${agree} de sub mesaj.`)
   .setColor("#1ABC9C")
-  //.setThumbnail("http://icons.iconarchive.com/icons/graphicloads/polygon/128/problem-2-icon.png")
+  .setThumbnail("http://icons.iconarchive.com/icons/graphicloads/polygon/128/problem-2-icon.png")
   .setTimestamp();
   let newbiechannel = message.guild.channels.find(`name`, newbie_channel);
   //if(!newbiechannel) return Funct.error(message, "Canalul pentru începători nu există.");
