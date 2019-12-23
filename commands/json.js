4583a2c3a696fb9b79163f317b07acdd27d7e2a9
HaZZe <32200147+HaZZeAdv@users.noreply.github.com>
2019-12-23 11:40:59 +0200

    Update json.js

--- commands/json.js
+++ commands/json.js
@@ -9,17 +9,12 @@ module.exports.run = async (bot, message, args) => {
         let body = r.body;
         let id = Number(args[0]);
         if(!id) return message.channel.send("Insereaza un ID.");
+        
         if(isNaN(id)) return message.channel.send("ID invalid");
 
         let entry = body.find(post => post.id === id);
-        if(!entry) return message.channel.send("Nu exista");
+        console.log(entry);
 
-        let embed = new Discord.RichEmbed()
-        .SetDescription(entry.body)
-        .AddField("Autor", entry.userId)
-        .SetFooter("Post ID: " + entry.id);
-
-    message.channel.send({embed:embed});
     });
 }
 
