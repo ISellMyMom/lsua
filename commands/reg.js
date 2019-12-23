252a3effbba59c064800839af2fd22e9ab205b08
HaZZe <32200147+HaZZeAdv@users.noreply.github.com>
2019-12-23 12:45:30 +0200

    Update and rename nush.js to reg.js

---
+++ commands/reg.js
@@ -0,0 +1,14 @@
+	
+const Discord = require("discord.js");
+const economy = require("./assets/regulament.json")
+const fs = require("fs");
+exports.run = async (bot, message, args) => {	
+	message.channel.send(Regula_1.descriere);
+}
+
+exports.help = {
+    name: "reg",
+    type: "info",
+    desc: "Test",
+    usage: "reg"
+}
