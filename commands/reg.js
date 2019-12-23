8651907009ec2ec6400b9ba87cc9b60144997c5f
HaZZe <32200147+HaZZeAdv@users.noreply.github.com>
2019-12-23 13:52:49 +0200

    Update reg.js

--- commands/reg.js
+++ commands/reg.js
@@ -3,18 +3,15 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const regulament = require("../assets/regulament.json")
 exports.run = async (bot, message, args) => {
-	let msg = message.content.join("@");
 	let reg = args.join("#");
 	console.log(regulament);
 	console.log(regulament.regula);
 	console.log(regulament.sanctiune.reg);
 	console.log(args[0]);
 	console.log(reg);
-	console.log(msg);
 	 var contents = fs.readFileSync("../assets/regulament.json");
 	 var jsonContent = JSON.parse(contents);
 	console.log(jsonContent.regula.reg);
-	console.log(jsonContent.regula.msg);
 }
 
 exports.help = {
