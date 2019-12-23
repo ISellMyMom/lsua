b2f5c1c572123b9077623bb2740d6146efa9d97a
HaZZe <32200147+HaZZeAdv@users.noreply.github.com>
2019-12-24 01:20:10 +0200

    Update duma123.js

--- commands/duma123.js
+++ commands/duma123.js
@@ -4,7 +4,7 @@ const duma = require("../assets/dume.json");
 
 module.exports.run = async (bot, message, args) => {
 
-  message.channel.send("Duma: " + duma.dume[Math.floor(Math.random() * duma.dume.length)]);
+  console.log(duma);
 }
 
 module.exports.help = {
