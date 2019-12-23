c1e3f4c08b14c9b1ad354343b32f31403b24511a
HaZZe <32200147+HaZZeAdv@users.noreply.github.com>
2019-12-23 14:13:23 +0200

    Update reg.js

--- commands/reg.js
+++ commands/reg.js
@@ -4,9 +4,11 @@ const fs = require("fs");
 const regulament = require("../assets/regulament.json")
 exports.run = async (bot, message, args) => {
 	let reg = "regula_".concat(args[0]);
+	const reg_nr = reg;
 	console.log(reg);
+	console.log(reg_nr);
 	console.log(regulament.regula_1);
-	console.log(regulament.reg);
+	console.log(regulament.reg_nr);
 	console.log(args[0]);
 
 }
