2ace7d8d0b9ccc5f719ef60d333e67d28c7b31f5
HaZZe <32200147+HaZZeAdv@users.noreply.github.com>
2019-12-23 12:49:07 +0200

    Update reg.js

--- commands/reg.js
+++ commands/reg.js
@@ -1,6 +1,6 @@
 	
 const Discord = require("discord.js");
-const economy = require("./assets/regulament.json")
+const economy = require("../assets/regulament.json")
 const fs = require("fs");
 exports.run = async (bot, message, args) => {	
 	message.channel.send(Regula_1.descriere);
